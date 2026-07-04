5af0fce49858423f1e1fc4c23a6b8e87015da645
avengsum <sy710481@gmail.com>
2026-07-04 13:52:39 +0530

    added cicd in our workflow

--- main.tf
+++ main.tf
@@ -1,18 +1,106 @@
-terraform {
-  required_providers {
-  local = {
-    source = "hashicorp/local"
-    version = "~> 2.5"
+############################
+# PUBLIC S3 BUCKET
+############################
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "secureshop-public-logs-demo"
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "logs" {
+
+  bucket = aws_s3_bucket.logs.id
+  acl    = "public-read"
+
+}
+
+############################
+# OPEN SECURITY GROUP
+############################
+
+resource "aws_security_group" "web" {
+
+  name = "web-security-group"
+
+  ingress {
+
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
-}
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
 
 }
 
-provider "local" {}
+############################
+# IAM USER
+############################
 
-resource "local_file" "hello" {
-  filename = "hello.txt"
-  content = "this is hello txt file to test terraform"
+resource "aws_iam_user" "developer" {
+
+  name = "developer"
 
 }
 
+resource "aws_iam_user_policy" "developer" {
+
+  name = "admin"
+
+  user = aws_iam_user.developer.name
+
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+
+        Effect = "Allow"
+
+        Action = "*"
+
+        Resource = "*"
+
+      }
+
+    ]
+
+  })
+
+}
+
+############################
+# HARDCODED SECRET
+############################
+
+resource "aws_ssm_parameter" "db_password" {
+
+  name  = "/prod/database/password"
+
+  type  = "SecureString"
+
+  value = "SuperSecret123"
+
+}
